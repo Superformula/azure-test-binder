5b77ae802bca994be2cb5818e280295ea6465230
James Dunnam <james.dunnam@superformula.com>
2020-09-28 19:21:07 -0400

    fix(testing): remove flaky test

--- src/config.test.ts
+++
@@ -1,9 +0,0 @@
-import { getEnv } from './config'
-
-describe('configuration test', function () {
-  it('should have the PAT in the environment', function () {
-    const env = getEnv()
-    expect(env.AZURE_PERSONAL_ACCESS_TOKEN).toBeTruthy()
-    expect(env.ORG_URL).toBeTruthy()
-  })
-})
