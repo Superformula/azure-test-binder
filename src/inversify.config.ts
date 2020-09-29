94b4988db7a832484121fbf4f9a791a5d8d31883
James Dunnam <69158017+jamesdunnam-sf@users.noreply.github.com>
2020-09-28 21:18:25 -0400

    Feature/ado task (#3)
    * feat(infrastructure): create ado task
    
    * cleanup(infrastructure): adding script to build az task vsix file
    
    * fix(testing): remove flaky test

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -2,15 +2,15 @@ import 'reflect-metadata'
 
 import { Container } from 'inversify'
 
-import { getEnv } from './config'
+import { EnvOptions, getEnv } from './config'
 import { Env, TYPES } from './types/types'
 import { bindings } from './workitem'
 
 /**
  * Create and configure a {@link Container}
  */
-export const initContainer = async (): Promise<Container> => {
-  const env = getEnv()
+export const getContainer = async (envOptions: EnvOptions): Promise<Container> => {
+  const env = getEnv(envOptions)
   const container = new Container()
   container.bind<Env>(TYPES.Env).toConstantValue(env)
   await container.loadAsync(bindings(env))
